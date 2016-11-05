bc5728c194ac173f21aaaba728bd6d53ede99748
Geoffrey Challen <challen@buffalo.edu>
2016-11-05 14:49:12 -0400

    Working.

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ var chai = require('chai'),
 		corequge = require('..');
 
 describe('corequge', function() {
-	it('should parse simple variable declarations', function (done) {
-		var questions = corequge('var x = 42');
+	it('should parse literal variable declarations', function (done) {
+		var questions = corequge('var answer = 42').variables();
 		done();
 	});
 });
