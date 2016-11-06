dcf1d4a69f65d4db320b1561dca1900425278537
Geoffrey Challen <challen@buffalo.edu>
2016-11-06 13:06:32 -0500

    Parsing declarations.

--- test/index.js
+++ test/index.js
@@ -3,7 +3,12 @@ var chai = require('chai'),
 
 describe('corequge', function() {
 	it('should parse literal variable declarations', function (done) {
-		var questions = corequge('var answer = 42').variables();
+		var questions = corequge(
+				'var answer = 42, test, broken = "me";' +
+				'var another = function() { return "blah" };' + 
+				'let third = 43;'
+			)
+			.variableInitialization();
 		done();
 	});
 });
